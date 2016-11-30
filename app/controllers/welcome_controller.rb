bd0a680c3923d6cf343a07088c60944838879a4c
Niesha Butler <niesha@sportsdbn.com>
2016-11-30 12:04:00 -0500

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,13 @@
+class WelcomeController < ApplicationController
+  def index
+  	@homestate = 'California'
+    @countries = ['Oman','Croatia','Peru']
+    @travel_pics = ['peru.jpg', 'peru2.jpg', 'peru3.jpg']
+
+  end
+  def about
+  	@color = 
+  	@size =
+  	@pic
+  end
+end
